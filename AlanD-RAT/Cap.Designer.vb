478e648d7add1be56fcb3d9a046a77eeb76aeccb
irahu <irahu@DESKTOP-T210NP3>
2022-05-24 21:41:06 +0200

    Agregar archivos de proyecto.

---
+++ AlanD-RAT/Cap.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Cap
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Cap))
+        Me.P1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.C1 = New System.Windows.Forms.ComboBox()
+        Me.C2 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.C = New System.Windows.Forms.NumericUpDown()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.P1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'P1
+        '
+        Me.P1.BackColor = System.Drawing.Color.Black
+        Me.P1.BackgroundImage = CType(resources.GetObject("P1.BackgroundImage"), System.Drawing.Image)
+        Me.P1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.P1.Location = New System.Drawing.Point(0, 0)
+        Me.P1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.P1.Name = "P1"
+        Me.P1.Size = New System.Drawing.Size(751, 430)
+        Me.P1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.P1.TabIndex = 0
+        Me.P1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("Corbel", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(16, 36)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Image Size:"
+        '
+        'C1
+        '
+        Me.C1.FormattingEnabled = True
+        Me.C1.Location = New System.Drawing.Point(21, 63)
+        Me.C1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.C1.Name = "C1"
+        Me.C1.Size = New System.Drawing.Size(108, 24)
+        Me.C1.TabIndex = 2
+        '
+        'C2
+        '
+        Me.C2.FormattingEnabled = True
+        Me.C2.Location = New System.Drawing.Point(21, 134)
+        Me.C2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.C2.Name = "C2"
+        Me.C2.Size = New System.Drawing.Size(108, 24)
+        Me.C2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.Font = New System.Drawing.Font("Corbel", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(16, 107)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Split:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.Font = New System.Drawing.Font("Corbel", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(16, 174)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Quality %"
+        '
+        'C
+        '
+        Me.C.Location = New System.Drawing.Point(21, 201)
+        Me.C.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.C.Name = "C"
+        Me.C.Size = New System.Drawing.Size(109, 22)
+        Me.C.TabIndex = 6
+        Me.C.Value = New Decimal(New Integer() {40, 0, 0, 0})
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.Color.Black
+        Me.CheckBox1.Font = New System.Drawing.Font("Corbel", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.ForeColor = System.Drawing.Color.Red
+        Me.CheckBox1.Location = New System.Drawing.Point(21, 245)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(104, 25)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Show Line"
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(21, 276)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 28)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.BackColor = System.Drawing.Color.Black
+        Me.CheckBox2.Font = New System.Drawing.Font("Corbel", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox2.ForeColor = System.Drawing.Color.Red
+        Me.CheckBox2.Location = New System.Drawing.Point(21, 326)
+        Me.CheckBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(135, 25)
+        Me.CheckBox2.TabIndex = 9
+        Me.CheckBox2.Text = "Control Mouse"
+        Me.CheckBox2.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 500
+        '
+        'Cap
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(751, 430)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.C)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.C2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.C1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.P1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Cap"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Remote Desktop"
+        CType(Me.P1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents P1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents C1 As System.Windows.Forms.ComboBox
+    Friend WithEvents C2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents C As System.Windows.Forms.NumericUpDown
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+End Class
